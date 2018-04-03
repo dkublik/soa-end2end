286a3d7f8c9c95346376fa5796fa034f7b52bbb3
dkublik <dkublik@gmail.com>
2018-04-03 21:47:41 +0200

    deserialization example

---
+++ rest-assured/src/test/java/pl/dk/soa/candidate/CandidatePersonalDetails.java
@@ -0,0 +1,22 @@
+package pl.dk.soa.candidate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CandidatePersonalDetails {
+
+    private String login;
+    private String name;
+    private String surname;
+    private String email;
+    private String phoneNumber;
+    private String photoPath;
+    private String personalDescription;
+
+}
---
+++ rest-assured/src/test/java/pl/dk/soa/candidate/DeserializationExample.java
@@ -0,0 +1,41 @@
+package pl.dk.soa.candidate;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.config.ObjectMapperConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static pl.dk.soa.Hosts.CANDIDATE_HOST;
+
+class DeserializationExample {
+
+    @Test
+    void shouldGetProfileDetails() {
+        // given
+        // to ignore address filed that we don't care about
+        RestAssured.config = RestAssuredConfig.config().objectMapperConfig(
+                new ObjectMapperConfig().jackson2ObjectMapperFactory((cls, charset) -> new ObjectMapper()
+                        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false))
+        );
+        RequestSpecification requestSpecification = given()
+                .pathParam("candidateId", "mHamill");
+
+        // when
+        Response response = requestSpecification.when().get(CANDIDATE_HOST + "/v0/candidates/profile/{candidateId}");
+
+        // then
+        response.then()
+                .statusCode(HttpStatus.OK.value());
+        CandidatePersonalDetails candidatePersonalDetails = response.as(CandidatePersonalDetails.class);
+        Assert.assertThat(candidatePersonalDetails.getLogin(), is("mhamill"));
+    }
+
+}
